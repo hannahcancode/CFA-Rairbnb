0f538b3b51fde0a8d990f8d64c094720059cc382
Hannah Thompson <thompson.h@gmail.com>
2017-04-04 11:32:33 +1000

    added Booking scaffold

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,5 @@ class Room < ApplicationRecord
   mount_uploaders :pictures, RoomUploader
   serialize :pictures, JSON # If you use SQLite, add this line.
   belongs_to :user
+  has_many :bookings
 end
