600d6b555a5e54a2aadc27a239ba3dbbb158db60
Hannah Thompson <thompson.h@gmail.com>
2017-04-04 15:20:35 +1000

    added some Stripe functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :bookings
   resources :rooms
+  resources :charges
   devise_for :users
   root 'pages#home'
+  get 'hostdashboard', to: 'pages#host_dashboard'
+  get 'guestdashboard', to: 'pages#guest_dashboard'
   get 'pages/rooms'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
